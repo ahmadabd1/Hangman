8406b1842a8afb340153018ecafb3f20c0cde7bd
Ahmad Abdallah <ahmadabdallah174@gmail.com>
2024-01-29 16:37:09 +0200

    add the icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 
 import './App.css';
-import Score from './components/Score';
+import Attemps from './components/Attemps';
 import Solution from './components/Solution';
 import { useState, useEffect } from 'react';
 
 function App() {
 
-  const words = ['react', 'javascript', 'hangman', 'Dom', 'Ahmad', 'ammerjmal', 'computer', 'python'];
+  const words = ['react', 'javascript', 'hangman', 'dom', 'ahmad', 'amerjmal', 'computer', 'python'];
   const [word, setWord] = useState('');
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [remainingAttempts, setRemainingAttempts] = useState(6);
+  const [img,setImage]=useState(`./hangmanicon/hangman-${0}.svg`)
+  const[score,setScore]=useState(100)
 
   useEffect(() => {
 
@@ -17,31 +19,28 @@ function App() {
     setWord(words[randomIndex]);
     // eslint-disable-next-line
   }, []);
-
+  
   const handleLetterClick = (letter) => {
+
     if (!guessedLetters.includes(letter)) {
       setGuessedLetters([...guessedLetters, letter]);
+      setScore(score+5)
       if (!word.includes(letter)) {
+        setScore(score-20)
         setRemainingAttempts(remainingAttempts - 1);
+        let numImg=(6-remainingAttempts)+1
+        setImage(`./hangmanicon/hangman-${numImg}.svg`)
       }
     }
   };
 
-  const displayWord = word
-    .split('')
-    .map((letter) => (guessedLetters.includes(letter) ? letter : '_'))
-    .join(' ');
-
-  const isGameOver =
-    remainingAttempts === 0 || !displayWord.includes('_');
+  const displayWord = word.split('').map((letter) => (guessedLetters.includes(letter) ? letter : '_')).join(' ');
+  const isGameOver = remainingAttempts === 0 || !displayWord.includes('_');
 
   const renderAlphabetButtons = () => {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
     return alphabet.split('').map((letter) => (
-      <button
-        key={letter}
-        onClick={() => handleLetterClick(letter)}
-        disabled={guessedLetters.includes(letter) || isGameOver} >
+      <button className='btn' key={letter} onClick={() => handleLetterClick(letter)} disabled={guessedLetters.includes(letter) || isGameOver} >
         {letter}
       </button>
     ));
@@ -53,16 +52,19 @@ function App() {
   return (
     <div className='App'>
       <h1>Hangman Game</h1>
-      <p>Remaining Attempts: <Score remainingAttempts={remainingAttempts}/></p>
+      <h2>Score : {score}</h2>
+      <p>Remaining Attempts: <Attemps remainingAttempts={remainingAttempts}/></p>
       <Solution displayWord={displayWord} />
       <div>Hint : It's Must be a Name or Coding Language.</div>
       <div>
         {renderAlphabetButtons()}
       </div>
       {isGameOver && (
-        <p>{remainingAttempts === 0 ? 'Game Over! Try again.' : 'You Win!'}</p>
+        <p>{remainingAttempts === 0 ? 'Game Over! Try again.' : 'You Win Congzzz!'}</p>
       )}
-      <button onClick={refreshPage}>restart</button>
+      <button className='restbtn' onClick={refreshPage}>restart</button>
+        <br></br><br></br>
+      <img src={img} alt=''></img>
     </div>
 
   );
--- src/components/Attemps.js
+++ src/components/Attemps.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Score({remainingAttempts}) {
   return (
-    <div>{remainingAttempts}</div>
+    <span>{remainingAttempts}</span>
   )
 }
